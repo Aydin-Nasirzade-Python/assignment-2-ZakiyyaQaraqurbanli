45e823ec2dc1eb6702a1df832d49ec2ed13a113d
ZakiyyaQaraqurbanli <165477511+ZakiyyaQaraqurbanli@users.noreply.github.com>
2024-04-05 22:52:30 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -3,13 +3,13 @@
 def main():
   a=input("Enter name of the month [ex. June]: "))
   b=int(input("Enter the day [ex. 5]: "))
-  if a=="April" or a=="May" or (a=="March" and 31>=b>=20) or (a=="June" and b<21):
+  if a=="April" or a=="May" or (a=="March" and 31>=b>=20) or (a=="June" and 1<=b<21):
     print(f"{a}{b} is in Spring")
-  elif (a=="June" and 31>b>=21) or a=="July" or a=="August" or (a=="September" and b<22):
+  elif (a=="June" and 31>b>=21) or a=="July" or a=="August" or (a=="September" and 1<=b<22):
     print(f"{a}{b} is in Summer")
-  elif (a=="September" and 31>b>=22) or a=="October" or a=="November" or (a=="December" and b<21):
+  elif (a=="September" and 31>b>=22) or a=="October" or a=="November" or (a=="December" and 1<=b<21):
     print(f"{a}{b} is in Fall")
-  elif (a=="December" and 31>=b>=21) or a=="January" or a=="February" or (a=="March" and b<20):
+  elif (a=="December" and 31>=b>=21) or a=="January" or a=="February" or (a=="March" and 1<=b<20):
     print(f"{a}{b} is in Winter")
   pass
 
