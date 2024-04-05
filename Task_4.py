c6f62f8e83022c506f80f0daebf7c7827ac7598f
ZakiyyaQaraqurbanli <165477511+ZakiyyaQaraqurbanli@users.noreply.github.com>
2024-04-06 00:15:39 +0400

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -1,9 +1,33 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  a=int(input("Enter the year [ex. 2021]: "))
+  if a<0:
+    print("Invalid year!")
+  elif a%12==0:
+    print(f"{a} is the year of the Monkey")
+  elif a%12==1:
+    print(f"{a} is the year of the Rooster")
+  elif a%12==2:
+    print(f"{a} is the year of the Dog")
+  elif a%12==3:
+    print(f"{a} is the year of the Pig")
+  elif a%12==4:
+    print(f"{a} is the year of the Rat")
+  elif a%12==5:
+    print(f"{a} is the year of the Ox")
+  elif a%12==6:
+    print(f"{a} is the year of the Tiger")
+  elif a%12==7:
+    print(f"{a} is the year of the Hare")
+  elif a%12==8:
+    print(f"{a} is the year of the Dragon")
+  elif a%12==9:
+    print(f"{a} is the year of the Snake")
+  elif a%12==10:
+    print(f"{a} is the year of the Horse")
+  elif a%12==11:
+    print(f"{a} is the year of the Sheep")
   pass
 
 if __name__ == "__main__":
